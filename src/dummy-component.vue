a26b03b4e13bc75c2ba674d9031b9f1ee3e8c1d1
Ijiekhuamen Rex <dev.rexijie@gmail.com>
2020-04-27 19:56:19 +0100

    added dummy component to test components with directive

---
+++ src/dummy-component.vue
@@ -0,0 +1,6 @@
+<template>
+  <section class="animated mb-5"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur exercitationem
+    inventore iure, molestias nisi odit praesentium quam, recusandae similique sunt veritatis, voluptas. Asperiores
+    dolores maxime minima recusandae reprehenderit sed ullam!
+  </section>
+</template>
